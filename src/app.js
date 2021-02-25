a85915f3cc20a366f1c9d873233e939e9de33f82
Chad Cleveland <56498633+LightbulbServices@users.noreply.github.com>
2021-02-25 10:32:01 -0800

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,10 @@
+// JavaScript file
+// Description
+// Author: Chad Cleveland. Lightbulb Web Services. www.Lightbulb.Services
+// Copyright: © Lightbulb Web Services 2021.
+
+// Created: 02/25/2021
+// Last modified: 02/25/2021 18:10:04
+
+var debugTimer = 0;
+let dTimer = setInterval(function() { debugTimer = debugTimer + 1; }, 100);
