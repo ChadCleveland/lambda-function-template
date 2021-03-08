b5b592934982b52a8b4ab9059dc63d3536bdc685
ChadASUS <chad@lightbulb.services>
2021-03-08 12:18:26 -0800

    Added dev and prod dirs

--- app/d/day-mon/js/app.js
+++ app/d/day-mon/js/app.js
@@ -3,7 +3,7 @@
 // Author: Chad Cleveland | Lightbulb Web Services | Lightbulb.Services
 // Copyright: © Lightbulb Web Services 2021. All Rights Reserved.
 
-// Last modified: 02/25/2021 18:10:04
+// Last modified: 03/03/2021 17:10:20
 
 // debugging and console
 var debugTimer = 0;
@@ -27,3 +27,21 @@ if (appVersion.indexOf('.com/') > -1) {
         appVersion = appVersion.split('/')[0];
     }
 }
+
+function getCountry() {
+    console.log('get country approach 2');
+    let fPath = 'https://lib.lightbulb.services/img/lws-ico-1.png';
+    let cXhr = new XMLHttpRequest();
+    cXhr.open('get', fPath);
+    cXhr.send();
+    cXhr.onload = function () {
+        console.log('img response');
+        // let r = cXhr.response;
+        let h = cXhr.getAllResponseHeaders();
+        // console.log('r', r);
+        console.log('h', h);
+    }
+
+}
+
+getCountry();
